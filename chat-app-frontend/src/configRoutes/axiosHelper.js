c295be659ef9f71fe66e3e040426ee0caed11067
Aman-Puskar <amanpuskaremg25@gmail.com>
2025-08-03 17:43:11 +0530

    changed to render url

--- chat-app-frontend/src/configRoutes/axiosHelper.js
+++ chat-app-frontend/src/configRoutes/axiosHelper.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// export const baseURL = "https://mychat-aaxu.onrender.com";
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://mychat-aaxu.onrender.com";
+// export const baseURL = "http://localhost:8080";
 
 export const  httpClient = axios.create({
     baseURL : baseURL
